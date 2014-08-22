ee63ea3cb1557dcf1e945367a29c65b48f40d8c9
Thomas Pelaia II <pelaiata@ornl.gov>
2014-08-22 11:47:35 -0400

    The query part of the URL Spec for web slides is parsed for the ditour-zoom key which takes one of "none", "width", "height" or "both". The zoom mode is then applied to the web page to scale it appropriately.

--- iLobby/ILobbyWebSlide.m
+++ iLobby/ILobbyWebSlide.m
@@ -6,8 +6,18 @@
 //  Copyright (c) 2014 UT-Battelle ORNL. All rights reserved.
 //
 
+@import JavaScriptCore;
+
 #import "ILobbyWebSlide.h"
 #import "ILobbySlideFamily.h"
+
+
+typedef enum : NSInteger {
+	ZOOM_NONE,
+	ZOOM_WIDTH,
+	ZOOM_HEIGHT,
+	ZOOM_BOTH
+} ZoomMode;
 
 
 static NSSet *WEB_EXTENSIONS;
@@ -16,6 +26,7 @@ static NSSet *WEB_EXTENSIONS;
 @interface ILobbyWebSlide () <UIWebViewDelegate>
 @property (assign) BOOL canceled;
 @property (strong) UIWebView *webView;
+@property (assign) ZoomMode zoomMode;
 @end
 
 
@@ -48,6 +59,34 @@ static NSSet *WEB_EXTENSIONS;
 	NSString *slideWebSpec = [[NSString stringWithContentsOfFile:self.mediaFile encoding:NSUTF8StringEncoding error:nil] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 	NSURL *slideURL = [NSURL URLWithString:slideWebSpec];
 
+	NSString *query = slideURL.query;
+	NSDictionary *queryDictionary = [ILobbyWebSlide dictionaryForQuery:query];
+//	NSLog( @"query dictionary: %@", queryDictionary );
+	NSString *zoomModeID = queryDictionary[@"ditour-zoom"];
+
+	if ( zoomModeID != nil ) {
+		zoomModeID = zoomModeID.lowercaseString;
+
+		if ( [zoomModeID isEqualToString:@"none"] ) {
+			self.zoomMode = ZOOM_NONE;
+		}
+		else if ( [zoomModeID isEqualToString:@"width"] ) {
+			self.zoomMode = ZOOM_WIDTH;
+		}
+		else if ( [zoomModeID isEqualToString:@"height"] ) {
+			self.zoomMode = ZOOM_HEIGHT;
+		}
+		else if ( [zoomModeID isEqualToString:@"both"] ) {
+			self.zoomMode = ZOOM_BOTH;
+		}
+		else {
+			self.zoomMode = ZOOM_NONE;
+		}
+	}
+	else {
+		self.zoomMode = ZOOM_BOTH;
+	}
+
 	CGRect viewSize = presenter.externalBounds;
 
 	self.webView = [[UIWebView alloc] initWithFrame:viewSize];
@@ -76,12 +115,37 @@ static NSSet *WEB_EXTENSIONS;
 	if ( !self.canceled && self.webView == webView ) {
 		CGSize contentSize = webView.scrollView.contentSize;
 
-		if ( contentSize.width > 0 ) {
+		if ( contentSize.width > 0 && contentSize.height > 0 ) {
 			CGSize viewSize = webView.bounds.size;
-			double zoomScale = viewSize.width / contentSize.width;
-			webView.scrollView.minimumZoomScale = zoomScale;
-			webView.scrollView.maximumZoomScale = zoomScale;
-			webView.scrollView.zoomScale = zoomScale;
+			double widthZoom = viewSize.width / contentSize.width;
+			double heightZoom = viewSize.height / contentSize.height;
+			double zoomScale = 1.0;
+
+			switch ( self.zoomMode ) {
+				case ZOOM_WIDTH:
+					zoomScale = widthZoom;
+					break;
+
+				case ZOOM_HEIGHT:
+					zoomScale = heightZoom;
+					break;
+
+				case ZOOM_BOTH:
+					// use the minimum zoom to fit both the width and height on the page
+					zoomScale = widthZoom < heightZoom ? widthZoom : heightZoom;
+					break;
+
+				default:
+					break;
+			}
+
+			if ( zoomScale != 1.0 ) {
+				[webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"document.body.style.zoom=%f", zoomScale]];
+			}
+
+//			webView.scrollView.minimumZoomScale = zoomScale;
+//			webView.scrollView.maximumZoomScale = zoomScale;
+//			webView.scrollView.zoomScale = zoomScale;
 		}
 	}
 }
@@ -107,5 +171,32 @@ static NSSet *WEB_EXTENSIONS;
 - (BOOL)isSingleFrame {
 	return YES;
 }
+
+
+// extract the key value pairs for the raw URL query and return then in a dictionary
++ (NSDictionary *)dictionaryForQuery:(NSString *)query {
+	if ( query != nil ) {
+		JSContext *scriptContext = [JSContext new];
+		NSArray *records = [query componentsSeparatedByString:@"&"];
+		NSMutableDictionary *dictionary = [NSMutableDictionary new];
+		for ( NSString *record in records ) {
+			NSArray *fields = [record componentsSeparatedByString:@"="];
+			if ( fields != nil && fields.count == 2 ) {
+				NSString *key = fields[0];
+				if ( key ) {
+					NSString *scriptCommand = [NSString stringWithFormat:@"decodeURIComponent( \"%@\" )", fields[1]];
+					JSValue *scriptResult = [scriptContext evaluateScript:scriptCommand];
+					dictionary[ key ] = scriptResult.toString;
+				}
+			}
+		}
+
+		return [NSDictionary dictionaryWithDictionary:dictionary];
+	}
+	else {
+		return [NSDictionary dictionary];
+	}
+}
+
 
 @end
